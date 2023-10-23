3cee67cd5b7b33f01ca78c1e3a7df256ac27a8dd
rxolve <rxolve@gmail.com>
2023-10-23 21:13:44 +0900

    add get mapping

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,37 +1,37 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-	id("org.springframework.boot") version "3.1.5"
-	id("io.spring.dependency-management") version "1.1.3"
-	kotlin("jvm") version "1.8.22"
-	kotlin("plugin.spring") version "1.8.22"
+    id("org.springframework.boot") version "3.1.5"
+    id("io.spring.dependency-management") version "1.1.3"
+    kotlin("jvm") version "1.8.22"
+    kotlin("plugin.spring") version "1.8.22"
 }
 
 group = "com.workswhy"
 version = "0.0.1-SNAPSHOT"
 
 java {
-	sourceCompatibility = JavaVersion.VERSION_17
+    sourceCompatibility = JavaVersion.VERSION_17
 }
 
 repositories {
-	mavenCentral()
+    mavenCentral()
 }
 
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter-web")
-	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-	implementation("org.jetbrains.kotlin:kotlin-reflect")
-	testImplementation("org.springframework.boot:spring-boot-starter-test")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
 tasks.withType<KotlinCompile> {
-	kotlinOptions {
-		freeCompilerArgs += "-Xjsr305=strict"
-		jvmTarget = "17"
-	}
+    kotlinOptions {
+        freeCompilerArgs += "-Xjsr305=strict"
+        jvmTarget = "17"
+    }
 }
 
 tasks.withType<Test> {
-	useJUnitPlatform()
+    useJUnitPlatform()
 }
--- src/main/kotlin/com/workswhy/running/RunningApplication.kt
+++ src/main/kotlin/com/workswhy/running/RunningApplication.kt
@@ -2,10 +2,55 @@ package com.workswhy.running
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+import java.util.*
 
 @SpringBootApplication
 class RunningApplication
 
 fun main(args: Array<String>) {
-	runApplication<RunningApplication>(*args)
+    runApplication<RunningApplication>(*args)
+}
+
+class Coffee(private var name: String) {
+    private val id: String = UUID.randomUUID().toString()
+
+    fun getId(): String {
+        return id
+    }
+
+    fun getName(): String {
+        return name
+    }
+
+    fun setName(name: String) {
+        this.name = name
+    }
+}
+
+@RestController
+@RequestMapping("/coffees")
+class CoffeeController {
+    private var coffees = listOf<Coffee>(
+        Coffee("Americano"),
+        Coffee("Latte"),
+        Coffee("Mocha")
+    )
+
+    @GetMapping("")
+    fun all(): List<Coffee> {
+        return coffees
+    }
+
+    @GetMapping("/{id}")
+    fun byId(@PathVariable id: String): Coffee? {
+        println("id1: $id")
+        return coffees.stream()
+            .filter { c: Coffee -> c.getId() == id }
+            .findFirst()
+            .orElse(null)
+    }
 }
